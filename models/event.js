4eeb7eb0196f6d6ce3c0de6c04da6218bb6bf191
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-04-07 17:27:11 -0400

    loading in data

--- models/event.js
+++ models/event.js
@@ -5,10 +5,27 @@ const mongoose = require('mongoose');
 const Event = new mongoose.Schema({
   _id: String,
   people_invited: String,
-  description: String,
   location: [String],
   organizer: String,
-  date: Date    // not sure this is how
+  date: Date,    // not sure this is how
+  description: String
+
+});
+
+// Taken from phase-1
+// Convert incoming time strings to Date objects
+Section.path('time').set(function(time) {
+  return new Date(`1/15/2020 ${time}`);
+});
+
+// Provide a 12-hour time string as a virtual property
+Section.virtual('time12').get(function() {
+  return this.time.toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric'});
+});
+
+// Provide a 24-hour time string as a virtual property
+Section.virtual('time24').get(function() {
+  return this.time.toLocaleTimeString('en-US', {hour12: false});
 });
 
 // Export the model
---
+++ reset.js
@@ -0,0 +1,40 @@
+// Script for setting up a database.
+const mongoose = require('mongoose');
+const connect = require('./db');
+const Event = require('./models/event');
+const Comment = require('./models/comment');
+const User = require('./models/user');
+
+// Connect to the database
+connect();
+
+// Model a collection of courses
+const events = [
+  new Event({_id: 'Raft Day', people_invited: 'Everyone', location: 'The River', organizer: 'OC', date: new Date("2019-09-05T16:00:00Z"), description:'A party at the river. Bring rafts and fun'}),
+  new Event({_id: 'Titus', people_invited: 'Ticket Owners', location: 'Titus Mountain', organizer: 'OC', date: new Date("2020-03-14T10:00:00Z"), description:'A party at Titus mountain. Tickets selling for $30'}),
+  new Event({_id: 'Senior Streak', people_invited: 'Seniors', location: 'The Quad', organizer: 'Seniors', date: new Date("2019-08-29T20:00:00Z"), description:'All seniors streak across the quad (dont tell the freshmen)'}),
+  new Event({_id: 'Spring Fest', people_invited: 'Everyone', location: 'Intramural Field', organizer: 'ACE', date: new Date("2020-04-15T17:00:00Z"), description:'Huge concert! No drinks allowed!'})
+
+];
+
+// Model a collection of sections
+const users = [
+  new User({_id: 'OC'}),
+  new User({_id: 'Seniors'}),
+  new User({_id: 'ACE'}),
+];
+
+const comments = [
+  new User({_id: 'Remi Leblanc', event: 'Raft Day', comment: 'Hope the weather is nice!'}),
+  new User({_id: 'Isaac Brinkman', event: 'Tirus', comment: '$30? What a rip off!'}),
+  new User({_id: 'Darren', event: 'Spring Fest', comment: "Who's the artist this year? I reccomend Remi Leblanc."}),
+];
+
+// Reset the database
+mongoose.connection.dropDatabase()
+  .then(() => Promise.all(events.map(e => e.save())))
+  .then(() => Promise.all(users.map(user => user.save())))
+  .then(() => Promise.all(comments.map(comment => comment.save())))
+  .then(() => mongoose.connection.close())
+  .then(() => console.log('Database is ready.'))
+  .catch(error => console.error(error.stack));
