5ebe47981a7cf59b4811260f79395f53ff0fea12
Remi LeBlanc <remwileblanc@gmail.com>
2020-04-09 18:06:24 -0400

    error

--- router.js
+++ router.js
@@ -10,35 +10,35 @@ const router = express.Router();
 // I HAVE NO IDEA WHICH OF THESE IS NEEDED OR WHAT THEY DO
 // I JUST COPY PAST
 
-// Check for admin status
-const authorize = function(request, response, next) {
-  if (request.session.admin) {
-    next(); // Fulfill the request
-  } else {
-    response.status(401).end();
-  }
-};
+// // Check for admin status
+// const authorize = function(request, response, next) {
+//   if (request.session.admin) {
+//     next(); // Fulfill the request
+//   } else {
+//     response.status(401).end();
+//   }
+// };
 
 // Handle events requests
 router.get('/events', events.index);
 router.get('/events/:id', events.retrieve);
-router.post('/events', authorize, events.create);
-router.delete('/events/:id', authorize, events.delete);
-router.put('/events/:id', authorize, events.update);
+// router.post('/events', authorize, events.create);
+// router.delete('/events/:id', authorize, events.delete);
+// router.put('/events/:id', authorize, events.update);
 
 // Handle comments requests
 router.get('/comments', comments.index);
 router.get('/comments/:id', comments.retrieve);
-router.post('/comments', authorize, comments.create);
-router.delete('/comments/:id', authorize, comments.delete);
-router.put('/comments/:id', authorize, comments.update);
+// router.post('/comments', authorize, comments.create);
+// router.delete('/comments/:id', authorize, comments.delete);
+// router.put('/comments/:id', authorize, comments.update);
 
 // Handle users requests
 router.get('/users', users.index);
 router.get('/users/:id', users.retrieve);
-router.post('/users', authorize, users.create);
-router.delete('/users/:id', authorize, users.delete);
-router.put('/users/:id', authorize, users.update);
+// router.post('/users', authorize, users.create);
+// router.delete('/users/:id', authorize, users.delete);
+// router.put('/users/:id', authorize, users.update);
 
 
 // Export the router
