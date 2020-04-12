5929c674859e58fb957fb5bf65fc13a56612dd0e
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-04-12 16:18:51 -0400

    controllers

--- controllers/events.js
+++ controllers/events.js
@@ -1,7 +1,25 @@
-module.exports.index = function(request, response) {
-  response.send('GET /events');
+// Controller for the course collection.
+const Course = require('../models/event');
+
+// GET /courses
+module.exports.index = function(request, response, next) {
+  Course.distinct('_id')
+    .then(courseIDs => response.redirect(`/events/${courseIDs[0]}`))
+    .catch(error => next(error));
 };
 
-module.exports.retrieve = function(request, response) {
-  response.send(`GET /events/${request.params.id}`);
+// GET /courses/:id
+module.exports.retrieve = function(request, response, next) {
+  const queries = [
+    Course.findById(request.params.id),
+    Course.distinct('_id')
+  ];
+
+  Promise.all(queries).then(function([course, courseIDs]) {
+    if (course) {
+      response.render('events/index', {event: event, eventIDs: eventIDs});
+    } else {
+      next(); // No such course
+    }
+  }).catch(error => next(error));
 };
