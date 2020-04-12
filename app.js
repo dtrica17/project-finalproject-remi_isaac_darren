bf518eab66480e554b6a0ca6ed20be8c260a0f1c
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-04-12 17:00:54 -0400

    events/index is not working

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(function(request, response, next) {
 
 // Redirect from the home page
 app.get('/', function(request, response) {
+  // events/index does not exsist
   response.redirect('/events/index');
 });
 
