b29e8583ddb4336c3692d841dc95dd9318ab87b8
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-04-24 15:56:08 -0400

    added an else

--- controllers/events.js
+++ controllers/events.js
@@ -8,8 +8,8 @@ const Comment = require('../models/comment');
 module.exports.index = function(request, response, next) {
   const order = request.query.sort || '_id'; // Default to sort by course
   Event.find().sort(order)
-    //.then(allEvents => response.redirect(`events/${allEvents[0]._id}`))
-    .then(allEvents => response.render("events/detail",{allEvents:allEvents}))
+    .then(allEvents => response.redirect(`events/${allEvents[0]._id}`))
+    //.then(allEvents => response.render("events/detail",{allEvents:allEvents}))
     .catch(error => next(error))
 };
 //Get /events/Calendar
@@ -31,7 +31,8 @@ module.exports.retrieve = function(request, response, next) {
       response.render('events/index', {event: eve, allEvents: allEvents, comments: comments});
     }
     else {
-      next(); // No such Event
+      .then(response.render("events/detail",{allEvents:allEvents})
+      //next(); // No such Event
     }
   }).catch(error => next(error));
 };
