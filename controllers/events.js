847dffa7f86b1565556664a7f170383b252f7bc2
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-04-24 15:53:56 -0400

    im getting closer I think

--- controllers/events.js
+++ controllers/events.js
@@ -10,7 +10,6 @@ module.exports.index = function(request, response, next) {
   Event.find().sort(order)
     //.then(allEvents => response.redirect(`events/${allEvents[0]._id}`))
     .then(allEvents => response.render("events/detail",{allEvents:allEvents}))
-
     .catch(error => next(error))
 };
 //Get /events/Calendar
