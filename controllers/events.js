d12bacd4f464c97756afa93dd292ab7bd3ec3e1e
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-04-26 13:23:09 -0400

    missing coma

--- controllers/events.js
+++ controllers/events.js
@@ -87,7 +87,7 @@ router.get('/edit/:id',function(req,res){
 router.get('/:id',function(req,res){
   const queries = [
     Event.findById(req.params.id),
-    Comment.find().where('event').equals(req.params.id)
+    Comment.find().where('event').equals(req.params.id),
     User.find().where(username).equals(req.session.user)
   ];
   Promise.all(queries).then(function([eve, comments, owner]) {
