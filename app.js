2ea9c591e142222e1134cd33cfda84bca8c93dd4
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-04-25 21:39:56 -0400

    require connnect-flash

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const connect = require('./db');
 const passport = require('passport');
 const config = require('./config/database');
 const mongoose = require('mongoose');
+const flash = require('connect-flash')
 
 mongoose.connect(config.database);
 // connect to db
