77a6ba41f68f4806e158239c2ff94e7a7fc75e34
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-04-26 13:04:33 -0400

    user not users

--- models/event.js
+++ models/event.js
@@ -1,6 +1,6 @@
 // Model for the event collection.
 const mongoose = require('mongoose');
-const User = require('../models/users');
+const User = require('../models/user');
 
 // Define the schema
 const Event = new mongoose.Schema({
