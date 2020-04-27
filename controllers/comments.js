da7c23510b1cf6ed3ec84119e722e8fa0a447a51
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-04-27 19:04:28 -0400

    adding comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -7,12 +7,31 @@ const express = require('express');
 const router = express.Router();
 
 
-// delete an event
+// delete a comment
 router.delete('/:id', function(req, res){
   let query = {_id:req.params.id}
   Comment.remove(query, function(err){
     if(err){console.log(err)}
     res.send('Success');
+  })
+})
+
+// post a new comment
+router.post('/add', function(req,res){
+  let comment = new Comment();
+  comment.author = req.session.user;
+  comment.event = req.body.name;
+  comment.comment = req.body.comment;
+
+
+  comment.save(function(err){
+    if(err){
+      console.log(err);
+      return;
+    }
+    else{
+      res.redirect('/');
+    }
   })
 })
 
