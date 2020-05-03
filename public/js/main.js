7197220c20b92f20153a988d491bc6e0ed139b35
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-05-02 20:02:51 -0400

    some confirms

--- public/js/main.js
+++ public/js/main.js
@@ -4,34 +4,39 @@ $(document).ready(function(){
   $('.delete-eve').on('click',function(e){
     $target = $(e.target);
     const id = $target.attr('data-id');
-    $.ajax({
-      type:'Delete',
-      url:'/events/'+id,
-      success: function(response){
-        alert('Deleting Event');
-        window.location.href='/';
-      },
-      error: function(err){
-        console.log(err);
-      }
+    if(confirm("Delete this event?"){
+      $.ajax({
+        type:'Delete',
+        url:'/events/'+id,
+        success: function(response){
+          window.location.href='/';
+        },
+        error: function(err){
+          console.log(err);
+        }
 
+      })
     })
+
   });
   $('.delete-comment').on('click',function(e){
     $target = $(e.target);
     const id = $target.attr('data-id');
-    $.ajax({
-      type:'Delete',
-      url:'/comments/'+id,
-      success: function(response){
-        alert('Deleting Comment');
-        window.location.href='/';
-      },
-      error: function(err){
-        console.log(err);
-      }
+    if(confirm("Delete this comment?"){
+      $.ajax({
+        type:'Delete',
+        url:'/comments/'+id,
+        success: function(response){
+          alert('Deleting Comment');
+          window.location.href='/';
+        },
+        error: function(err){
+          console.log(err);
+        }
 
+      })
     })
+
   });
   $('.edit-event').on('click',function(e){
     $target = $(e.target);
