af0f1b9efaed7ece7bf0cbef7a5531dbcabfaef4
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-05-06 19:48:27 -0400

    main.js was adding the event Idk why

--- public/js/main.js
+++ public/js/main.js
@@ -48,18 +48,18 @@ $(document).ready(function(){
   });
 
 
-  $('.edit-event').on('click',function(e){
-    $target = $(e.target);
-    const id = $target.attr('data-id');
-    $.ajax({
-      type:'Get',
-      url:'/events/add',
-      success: function(response){
-        window.location.href='/events/add';
-      },
-      error: function(err){
-        console.log(err);
-      }
-    });
-  });
+  // $('.edit-event').on('click',function(e){
+  //   $target = $(e.target);
+  //   const id = $target.attr('data-id');
+  //   $.ajax({
+  //     type:'Get',
+  //     url:'/events/add',
+  //     success: function(response){
+  //       window.location.href='/events/add';
+  //     },
+  //     error: function(err){
+  //       console.log(err);
+  //     }
+  //   });
+  // });
 });
